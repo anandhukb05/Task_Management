011c9202414332d939197d31b11989b8597f00dc
anandhukb05 <anandhubijuabk7598@gmail.com>
2026-01-13 10:22:14 +0530

    tasks serializer added

---
+++ apps/tasks/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+        read_only_fields = ['assigned_to']
+
+class TaskCompleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['status', 'completion_report', 'worked_hours']
+
+    def validate(self, data):
+        if data.get('status') == 'completed':
+            if not data.get('completion_report') or not data.get('worked_hours'):
+                raise serializers.ValidationError(
+                    "Completion report and worked hours are required."
+                )
+        return data
